08fa3de7957192c4b71f152b16baa539a5df80b3
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2023-01-02 23:56:22 +0500

    update

--- Week04/task13.cpp
+++ Week04/task13.cpp
@@ -7,21 +7,24 @@ main()
   int workingday;
   int timeforgame;
   int difference;
+  int hour , mint;
   cout << "Enter Number of Holidays: ";
   cin >> holiday;
   workingday = 365 - holiday;
   timeforgame = (workingday * 63) + (holiday * 127);
   difference = 30000 - timeforgame;
+  hour = difference / 60;
+  mint = difference % 60;
   cout << endl;
   if(difference > 0)
   {
     cout << "Tom sleeps well ";
-    cout << difference << " minutes less for play.";
+    cout << hour << " hours and " << mint <<" minutes less for play.";
   }
   if(difference < 0)
   {
     cout << "Tom will run away ";
-    cout << -1 * difference << " minutes for play.";
+    cout <<-1 * hour << " hours and " <<-1 * mint <<" for play.";
   }    
 
 
